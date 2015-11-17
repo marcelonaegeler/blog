6d09bc43c5048aa4237f35fa2b7b474a90a01872
Marcelo A Naegeler <marcelo.andre.naegeler@gmail.com>
2015-11-17 17:58:02 -0200

    Performance enhancements and manifest adjust

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var exp = require( 'express' )
 	, app = exp();
 
-app.use( '/build', exp.static( './public/build' ) );
-app.use( '/images', exp.static( './public/images' ) );
+app.use( exp.static( './public' ) );
 app.use( '/vendor', exp.static( './node_modules' ) );
 
 app.set( 'view engine', 'jade' );
