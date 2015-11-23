28fea18cdde59f0119ef415c8730a706cc15ed23
Marcelo A Naegeler <marcelo.andre.naegeler@gmail.com>
2015-11-23 12:40:50 +0000

    Added posts controller

---
+++ controllers/posts.js
@@ -0,0 +1,122 @@
+( function () {
+	var posts = [
+		{
+			title: 'Post 01'
+			, date: new Date()
+			, author: 'Marcelo'
+			, content: 'Lorem ipsum sit dolor amet'
+			, link: '/admin/post/01'
+		}
+		, {
+			title: 'Post 02'
+			, date: new Date()
+			, author: 'Autor 02'
+			, content: 'Lorem ipsum sit dolor amet'
+			, link: '/post/02'
+		}
+		, {
+			title: 'Post 03'
+			, date: new Date()
+			, author: 'Autor 03'
+			, content: 'Lorem ipsum sit dolor amet'
+			, link: '/admin/post/03'
+		}
+		, {
+			title: 'Post 04'
+			, date: new Date()
+			, author: 'Autor 04'
+			, content: 'Lorem ipsum sit dolor amet'
+			, link: '/admin/post/04'
+		}
+		, {
+			title: 'Post 05'
+			, date: new Date()
+			, author: 'Autor 05'
+			, content: 'Lorem ipsum sit dolor amet'
+			, link: '/admin/post/05'
+		}
+	];
+	
+	var getAll = function ( req, res ) {
+
+		var query = {
+			posts: posts
+			, admin: req.isAdmin
+		};
+
+		if ( req.ajax ) {
+			return res.send( query );
+		} else {
+			return res.render( 'admin', query );
+		}
+
+	};
+
+	var getPost = function ( req, res ) {
+		
+		var postId = req.params.id;
+
+		var query = {
+			post: posts[ postId ]
+			, admin: req.isAdmin
+		};
+
+		if ( req.ajax ) {
+			return res.send( query );
+		} else {
+			return res.render( 'admin', query );
+		}		
+
+	};
+
+	var newPost = function ( req, res ) {
+
+		var data = {
+			title: req.params.title
+			, content: req.params.content
+			, date: new Date()
+		};
+
+		console.log( data );
+
+		var query = {
+			admin: req.isAdmin
+		};
+
+		if ( req.ajax ) {
+			return res.send( query );
+		} else {
+			return res.render( 'admin', query );
+		}
+	};
+
+	var editPost = function ( req, res ) {
+
+		var data = {
+			title: req.params.title
+			, content: req.params.content
+			, date: new Date()
+		};
+
+		console.log( data );
+
+		var query = {
+			admin: req.isAdmin
+		};
+
+		if ( req.ajax ) {
+			return res.send( query );
+		} else {
+			return res.render( 'admin', query );
+		}
+	};
+
+	var response = {
+		getAll: getAll
+		, getPost: getPost
+		, newPost: newPost
+		, editPost: editPost
+	};
+
+	return module.exports = response;
+})();
--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.use( '/vendor', exp.static( './node_modules' ) );
 app.set( 'view engine', 'jade' );
 app.set( 'views', './views' );
 
+
+app.use( function ( req, res, next ) {
+	
+	req.ajax = ( typeof req.headers[ 'x-request-with' ] !== 'undefined' );
+
+	req.isAdmin = true;
+
+	next();
+
+});
+
 app.use( '/', require( './routes/blog' ) );
 app.use( '/admin', require( './routes/admin' ) );
 
